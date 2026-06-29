75eb92839cf7f457635c930e145779db0b27d2da
Brave Developments <89262662+Brave-developments@users.noreply.github.com>
2026-06-29 17:04:04 +1000

    Add files via upload

--- shared/config.lua
+++ shared/config.lua
@@ -112,7 +112,7 @@ Config.Locations = {
         label            = 'Get your starter pack',                                 -- Target label (shown to the player)
         icon             = 'fa-solid fa-gift',                                      -- Target icon (from FontAwesome)
 
-        coords           = vec4(-1040.479126, -2731.582520, 20.164062, 238.110229), -- Coordinates and heading of the NPC (ped)
+        coords           = vector4(220.08, -954.28, 30.32, 19.52), -- Coordinates and heading of the NPC (ped)
 
         ped              = {                                                        -- Settings for the ped (NPC)
             model = 'a_m_y_business_03',                                            -- Ped model (see the FiveM ped model list)
@@ -134,13 +134,11 @@ Config.Locations = {
             enable = true,           -- Enable or disable the starter vehicle
             model = 'adder',         -- Vehicle model (from FiveM vehicle model list) will be ignored if random_vehicle is set to true
             random_vehicle = true,   -- Spawn a random vehicle from the list (true/false)
-            teleport_player = false, -- Teleport player to the vehicle (true/false)
+            teleport_player = true, -- Teleport player to the vehicle (true/false)
             vehicle_spawns = {       -- Spawn points for the vehicle (multiple spawn locations)
-                vec4(-1039.02, -2727.53, 19.65, 243.17),
-                vec4(-1043.3, -2725.09, 19.65, 241.12),
-                vec4(-1047.57, -2722.66, 19.65, 240.54),
-                vec4(-1034.38, -2719.0, 19.65, 240.52),
-                vec4(-1038.51, -2716.53, 19.64, 240.34),
+                vector4(237.52, -957.84, 28.76, 160.68),
+                vector4(241.76, -946.12, 28.76, 160.32),
+                vector4(234.76, -965.68, 28.76, 157.8),
             },
             fuel = 100.0, -- Fuel level of the vehicle when spawned
         },
@@ -168,11 +166,15 @@ Config.RandomVehicles = { -- list of vehicles to be given randomly to the player
 
 Config.StarterPackItems = { -- items that will be given to player
     ["normal"] = {
-        { item = 'burger',   amount = 5 },
-        { item = 'sprunk',   amount = 5 },
-        { item = 'phone',    amount = 1 },
-        { item = 'lockpick', amount = 5 },
-        { item = 'money',    amount = 5000 },
+        { item = 'skateboard',   amount = 1 },
+        { item = 'burger',   amount = 15 },
+        { item = 'water',   amount = 15 },
+        { item = 'lockpick', amount = 15 },
+        { item = 'money', amount = 50000 },
+        { item = 'phone', amount = 1 },
+        { item = 'chargingcable', amount = 1 },
+        { item = 'powerbank', amount = 1 },
+      
     },
     -- add more starter pack types here
 }
@@ -185,12 +187,10 @@ Config.SetFuel = function(vehicle, fuel)
         exports['LegacyFuel']:SetFuel(vehicle, fuel)
     elseif GetResourceState("cdn-fuel") == "started" then
         exports['cdn-fuel']:SetFuel(vehicle, fuel)
-    elseif GetResourceState("ps-fuel") == "started" then
-        exports['ps-fuel']:SetFuel(vehicle, fuel)
+    elseif GetResourceState("lc_fuel") == "started" then
+        exports['lc_fuel']:SetFuel(vehicle, fuel)
     elseif GetResourceState("lj-fuel") == "started" then
         exports['lj-fuel']:SetFuel(vehicle, fuel)
-    elseif GetResourceState("ox_fuel") == "started" then
-        Entity(vehicle).state.fuel = fuel
     else
         warn("Fuel resource not found, please set your fuel resource in the config.lua")
         SetVehicleFuelLevel(vehicle, fuel) -- Fallback to the default fuel system
